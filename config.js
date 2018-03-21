ea25e38350b4cdd79be22bfa1dc58fc22348b547
npool.pw <36919430+npoolpw@users.noreply.github.com>
2018-03-21 11:25:37 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,24 @@
-var api = "http://apihost.com:port";
+var api = "http://ca.npool.pw:2240";
 
-var poolHost = "apihost.com";
+var poolHost = "ca.npool.pw";
 
-var blockExplorer = "http://blockexplorer.pw/coin";
+var blockExplorer = "https://explorer.lines.pw/";
 
-var cryptonatorWidget = ["BTC-USD", "XMR-USD"];
+var cryptonatorWidget = ["BTC-USD"];
 
 var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
 
-var blockchainExplorer = "http://blockexplorer.pw/{symbol}/?hash={id}#blockchain_block";
+var blockchainExplorer = "https://explorer.lines.pw/?hash={id}#blockchain_block";
 
-var transactionExplorer = "http://blockexplorer.pw/{symbol}/?hash={id}#blockchain_transaction";
+var transactionExplorer = "https://explorer.lines.pw/?hash={id}#blockchain_transaction";
 
 var themeCss = "themes/default-theme.css";
 
 var networkStat = [
-        ["coin.apihost.com", "http://coin.apihost.com:port"]
+        ["npool.pw/linescoin", "http://ca.npool.pw:2240"],
+        ["pool.lines.pw", "https://explorer.lines.pw/API"],
+        ["cryptoknight.cc/lines", "http://94.130.207.37:40004"],
+        ["lines.fatpanda.club", "http://54.148.101.99:8016"],
+        ["youpool.io/LNS", "http://118.178.122.224:8110"],
+        ["lines.hpool.net", "http://server1.chinanorth.cloudapp.chinacloudapi.cn:18702"]
 ];
